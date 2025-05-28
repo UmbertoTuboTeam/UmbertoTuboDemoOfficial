e0258355a153023f1dad03d74ebb1a933dc5b687
UmbertoTuboTeam <umbertotubos@gmail.com>
2025-05-28 20:16:15 +0200

    Add files via upload

---
+++ GeneralStuff.js
@@ -0,0 +1,3 @@
+console.log('per quale motivo hai aperto la console? Non ti serve!');
+console.log("messagio dal creatore del sito: Non ti serve la console, l'unico motivo per aprirla è scrivere codici! Chiudi subito!");
+window.alert('!ATTENZIONE!: Questa è una primissima demo, ed è piu tosto diversa dal prodotto finale che rilascerò');
